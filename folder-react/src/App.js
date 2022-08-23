d3f82bd15a6a40787bb756b26baac92eca9c1b45
Na-Ku <naku3571@gmail.com>
2022-08-23 11:58:48 +0700

    membuat branch baru dan file baru, Todo dan TodoList beserta file css nya, ini bagian coba-coba

--- folder-react/src/App.js
+++ folder-react/src/App.js
@@ -1,52 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import FirstButton  from './componen/firstButton';
-import Navbar from './componen/Navbar';
-import Main from './componen/Main';
-import Footer from './componen/Footer';
+import Todo from  './Components2/Todo/Todo'
 
 
-function App() {
-
-  const Paraf = () => {
-    return (
-      <div>
-        <i>mantapppu!!</i>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, numquam!</p>
-      </div>
-    );
-  };
-  const NavHead = "Kolom Navigasi";
-  const klik = () => {
-    return alert('kimak kau');
-  }
-
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <Navbar NavHead={NavHead}/>
-
-        <img src={logo} className="App-logo" alt="logo" />
-
-        {/* <Main name="Fajar"/>
-        <Main name="Aleks"/>
-        <Main name="Ruben"/> */}
-        <p>
-          Xixixixigti <code>src/App.js</code> and save to reloa & halo cuy.
-        </p>
-
-        <FirstButton klik={klik} />
-
-        <Footer Paraf={Paraf}/>
-      </header>
-
-      <div className='container'>
-        <Main Parafg = {Element }  />
-      </div>
+      <Todo />
     </div>
-
-  );
+  )
 }
 
 export default App;
---
+++ folder-react/src/Components2/Todo/Todo.js
@@ -0,0 +1,13 @@
+import './Todo.css'
+import TodoList from '../TodoList/TodoList'
+
+const Todo = () => {
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoList />
+        </div>
+    )
+}
+
+export default Todo;
