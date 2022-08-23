1d10df34f775fc736dfea93f4d9b0ecc82dafdeb
Na-Ku <naku3571@gmail.com>
2022-08-23 12:54:43 +0700

    edit belom kelar pada branch coba1

--- folder-react/src/Components2/Todo/Todo.js
+++ folder-react/src/Components2/Todo/Todo.js
@@ -2,10 +2,18 @@ import './Todo.css'
 import TodoList from '../TodoList/TodoList'
 
 const Todo = () => {
+
+    
+    const List = [
+            {id: 1, tittle: 'Eat'},
+            {id: 2, tittle: 'Sleep'},
+            {id: 3, tittle: 'Code'},
+        ];
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList />
+            <TodoList dataTodos = {List} />
         </div>
     )
 }
