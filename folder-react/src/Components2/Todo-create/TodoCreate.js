7f5aa059d5c0548534105c3c7b33e1f943f5c31a
Na-Ku <naku3571@gmail.com>
2022-08-24 20:54:06 +0700

    membuat todo list dengan menambahkan suatu inputan baru, namun masih ada kekurangannya yaitu belum bisa memasukkan inputan dari user. Nanti baru dilanjutkan lagi

--- folder-react/src/Components2/Todo-create/TodoCreate.js
+++ folder-react/src/Components2/Todo-create/TodoCreate.js
@@ -1,19 +1,24 @@
+// import { useState } from 'react'
 import './TodoCreate.css'
 
 const TodoCreate = (props) => {
+
+    // const [getInput, setInput] = useState('')
+
     const handleSubmit = (event) => {
         event.preventDefault()
 
         const newTodo = {
             id: Math.floor(Math.random() + 10) + 1,
-            title: 'lear React'
+            title: 'kimak kau'
         }
 
-    props.onCreateTodo(newTodo);
+        props.onCreateTodo(newTodo);
     }
 
     return (
         <form className="todo-form" onSubmit = {handleSubmit}>
+            {/* <input type="text" onChange={handleInputTodo}/> */}
             <input type="text"/>
             <button type="submit">add</button>
         </form>
--- folder-react/src/Components2/Todo/Todo.js
+++ folder-react/src/Components2/Todo/Todo.js
@@ -1,23 +1,25 @@
+import { useState } from 'react'
 import './Todo.css'
 import TodoList from '../TodoList/TodoList'
 import TodoCreate from '../Todo-create/TodoCreate'
 
 const Todo = () => {
-    const ListTodo = [
+    const [getListTodo, setListTodo] = useState ( [
             {id: 1, title: "Eat"},
             {id: 2, title: "Sleep"},
             {id: 3, title: "Code"},
-        ]
+        ] ) 
 
     const eventCreateTodo = (todo) => {
-        ListTodo.push(todo);
+        setListTodo(getListTodo.concat(todo))
+        // console.log(ListTodo);
     }
 
     return (
         <div>
             <h3>Todo List</h3>
             <TodoCreate onCreateTodo={eventCreateTodo}/>  
-            <TodoList dataTodos = {ListTodo} />
+            <TodoList dataTodos = {getListTodo} />
         </div>
     )
 }
