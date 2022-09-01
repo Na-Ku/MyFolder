d1b382edbdcdad41200de59035331f6ddbea43c8
Na-Ku <naku3571@gmail.com>
2022-09-01 20:40:05 +0700

    membuat folder baru, folder components, dan juga mempraktikan atribut atau props

--- folder-react/src/App.js
+++ folder-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Intro from './components/intro';
 
 const App = () => {
 
   return (
       <div>
-        
+        <Intro jasa="Masak" />
+        <Intro jasa="Makan" />
       </div>
   );
 }
---
+++ folder-react/src/components/intro.js
@@ -0,0 +1,11 @@
+
+const Intro=(props) => {
+    return(
+        <div>
+            <h4>Intro</h4>
+            <p>Jasa yang ditawarkan : {props.jasa} </p>
+        </div>
+    )
+}
+
+export default Intro;
