98bc7b351e9a0b1d496ea7993020ab53b21b8490
Na-Ku <naku3571@gmail.com>
2022-09-01 20:15:16 +0700

    coba2 ke dua

--- folder-react/src/App.js
+++ folder-react/src/App.js
@@ -2,15 +2,9 @@ import './App.css';
 
 const App = () => {
 
-
   return (
-      <div className="App">
-        <header className='App-header'>
-          <nav>Test Pertama</nav>
-        </header>
-        <section>
-          <h2>Hello Dun!</h2>
-        </section>
+      <div>
+        
       </div>
   );
 }
