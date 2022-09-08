3b4d2ed8fa90c7bc1472f3567c08f42174b899b8
Na-Ku <naku3571@gmail.com>
2022-09-08 11:25:00 +0700

    memulai HOOK-useReff

--- folder-react/src/App.js
+++ folder-react/src/App.js
@@ -1,34 +1,9 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
 // import Intro from './components/intro';
 
 const App = () => {
 
-  const [getValue, setValue] = useState(0)
-  const [getNewValue, setNewValue] = useState(" ")
-
-  useEffect ( () => {
-    if (getValue > 0 && getValue < 5) {
-      setNewValue("Baru")
-    } else if (getValue >= 5) {
-      setNewValue("Baru Lagi")
-    } else {
-      setNewValue("Kosong")
-    }
-  }, [getValue] )
-
-  return (
-      <div>
-        <h5>Nilai Baru: {getNewValue}</h5>
-        <h3>Jumlah Sekarang adalah : {getValue} </h3>
-        <button onClick={() => setValue((prev) => prev + 1  ) } >Penambahan</button>
-        <button onClick={() => setValue((prev) => prev  - 1  ) } >Pengurangan</button>
-
-        {/*<Intro jasa="Masak" />
-        <Intro jasa="Makan" /> */}
-        
-      </div>
-  );
+  
 }
 
 export default App;
