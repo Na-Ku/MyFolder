1f0af6c4d7fd3e71f4f58c75fbcab5f24e38c275
Na-Ku <naku3571@gmail.com>
2022-09-27 19:09:42 +0700

    bar navigasi selesai

--- tes boot/JSX-JS/project-bwm1/src/components/Nav.js
+++ tes boot/JSX-JS/project-bwm1/src/components/Nav.js
@@ -6,33 +6,34 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navb = () => {
-    return(
-        <Navbar bg="primary" text="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  return (
+    // <Navbar bg="primary" variant="dark" expand="lg"> Jadi, react-bootstrap ini bisa dikombinasikan dengan syntax bootstrap biasa.
+    <Navbar className="navbar bg-primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#" className="fw-bold">
+          Sekolah-Ku
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+          <Nav className="me-auto my-2 my-lg-0 fw-semibold" style={{ maxHeight: "100px" }} navbarScroll>
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+            <Nav.Link href="#action2">Season</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Link Nonton</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">My Anime List</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#">Event</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-            <Button variant="outline-success" text="light">
-              Search
-            </Button>
+            <Button variant="danger">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
-}
+  );
+};
 
 export default Navb;
