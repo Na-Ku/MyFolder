313668ceebf04aae3b0bfc2f43e60004e058362f
Na-Ku <naku3571@gmail.com>
2022-09-28 18:27:21 +0700

    membuat file fil.css untuk backgorund, dan mungkin nanti beberapa editan di luar dari framework akan dibuat di file ini

--- tes boot/JSX-JS/project-bwm1/src/App.js
+++ tes boot/JSX-JS/project-bwm1/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
+import "./components/fil.css";
 // import dari react-bootstrap
 import Navb from "./components/Nav";
 
-
 function App() {
-  return (
-    <Navb/>
-  );
+  return <Navb />;
 }
 
 export default App;
