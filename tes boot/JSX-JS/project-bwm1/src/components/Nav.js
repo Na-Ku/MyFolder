91e172c96c0cdca21fd25736b3a6ee6bd51b8720
Na-Ku <naku3571@gmail.com>
2022-09-28 16:20:08 +0700

    mengganti urutan navbar dan juga isi dari navbar

--- tes boot/JSX-JS/project-bwm1/src/components/Nav.js
+++ tes boot/JSX-JS/project-bwm1/src/components/Nav.js
@@ -17,14 +17,14 @@ const Navb = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0 fw-semibold" style={{ maxHeight: "100px" }} navbarScroll>
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Season</Nav.Link>
+            <Nav.Link href="#action2">Sinopsis</Nav.Link>
+            <Nav.Link href="#">Event</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Link Nonton</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Season</NavDropdown.Item>
               <NavDropdown.Item href="#action4">My Anime List</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Link Nonton</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#">Event</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
