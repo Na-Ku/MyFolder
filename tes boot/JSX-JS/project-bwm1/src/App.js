534958bee2c40b3e3e18a8a3e2db953870c8e0a1
Na-Ku <naku3571@gmail.com>
2022-10-05 20:06:25 +0700

    membuat file untuk footer, tapi masih belum selesai di edit. Perlu diedit lebih bagus lagi

--- tes boot/JSX-JS/project-bwm1/src/App.js
+++ tes boot/JSX-JS/project-bwm1/src/App.js
@@ -3,13 +3,14 @@ import "./components/fil.css";
 // import component
 import Navb from "./components/Nav";
 import Cont from "./components/Main";
-
+import Foot from "./components/footer";
 
 function App() {
   return (
     <div>
       <Navb fluid />
-      <Cont/>
+      <Cont />
+      <Foot />
     </div>
   );
 }
---
+++ tes boot/JSX-JS/project-bwm1/src/components/footer.js
@@ -0,0 +1,9 @@
+const Foot = () => {
+  return (
+    <footer className="bg-dark mt-5 mb-0" variant="dark">
+      <h2>Thankyuu All</h2>
+    </footer>
+  );
+};
+
+export default Foot;
