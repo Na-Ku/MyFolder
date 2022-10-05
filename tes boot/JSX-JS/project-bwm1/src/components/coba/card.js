212586fe4b96decf04b8c2dcf9b20fcd37d452d8
Na-Ku <naku3571@gmail.com>
2022-10-05 20:41:23 +0700

    membuat Oject yang diubah-ubah menggunakan state pakai useState di file card.js

--- tes boot/JSX-JS/project-bwm1/src/components/coba/card.js
+++ tes boot/JSX-JS/project-bwm1/src/components/coba/card.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
+
 const Cards = () => {
-    return(
-        <div className="row row-cols-1 row-cols-md-3 g-4 mt-4">
-        <div className="col">
-          <div className="card h-100">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+  let [getMtr, setMtr] = useState({
+    // Object dalam bentuk Property
+    makan: "belum",
+    minum: "belum",
+    energi: 50,
+
+    // Object dalam bentuk Function
+    tambah: () => {
+      setMtr((data) => {
+        return {
+          ...data,
+          makan: "sudah",
+          energi: data.energi + 25,
+        };
+      });
+    },
+  });
+
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4 mt-4">
+      <div className="col">
+        <div className="card h-100">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Ini adalah contoh</p>
+            <ul>
+              <li>Makan : {getMtr.makan}</li>
+              <li>Minum : {getMtr.minum}</li>
+              <li>Energi : {getMtr.energi}</li>
+            </ul>
+            <button onClick={() => getMtr.tambah()} type="button" className="btn btn-outline-warning">
+              Tambah Energi
+            </button>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Cards;
