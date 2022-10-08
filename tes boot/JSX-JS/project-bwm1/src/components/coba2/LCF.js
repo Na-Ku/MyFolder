52d0f98f78e03dd6640851e74071cf809cf965db
Na-Ku <naku3571@gmail.com>
2022-10-08 20:07:25 +0700

    masih dalam percobaan

---
+++ tes boot/JSX-JS/project-bwm1/src/components/coba2/LCF.js
@@ -0,0 +1,27 @@
+// import React, { useEffect, useState } from 'react'
+
+// function LCF() {
+
+//     const [name, setName] = useState("");
+//     const [upDate, setUpDate] = useState(false);
+
+//     useEffect( () => {
+//         compDiMount() {
+//             fetch("https://jsonplaceholder.typicode.com/users/1")
+//                 .then((response) => response.json())
+//                 .then( (json)=> {
+//                     setName(name: json.name)
+//                 })
+//         }
+//     }, [] )
+
+//   return (
+//     <div>
+//       <h3>Name : {name}</h3>
+//     </div>
+//   )
+
+
+// }
+
+// export default LCF
